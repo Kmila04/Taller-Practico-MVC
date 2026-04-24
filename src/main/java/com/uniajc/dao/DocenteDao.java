01abe502ce63560232c8b0072a0679f6639399d9
Juan Manuel <juanmanueldaza69@gmail.com>
2026-04-24 16:29:27 -0500

    se agrega clases Dao

--- src/main/java/com/uniajc/dao/DocenteDao.java
+++ src/main/java/com/uniajc/dao/DocenteDao.java
@@ -1,5 +1,35 @@
 package com.uniajc.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import com.uniajc.config.ConexionDatabase;
+import com.uniajc.modelo.Docente;
+
 public class DocenteDao {
-    
+
+    public void guardarDocente(Docente docente) {
+
+        try {
+
+            Connection conexion = ConexionDatabase.conectar();
+
+            String sql = "INSERT INTO docente VALUES (?, ?, ?)";
+
+            PreparedStatement ps = conexion.prepareStatement(sql);
+
+            ps.setInt(1, docente.getId_docente());
+            ps.setString(2, docente.getNombre());
+            ps.setString(3, docente.getEspecialidad());
+
+            ps.executeUpdate();
+
+            System.out.println("Docente guardado correctamente");
+
+        } catch (SQLException e) {
+
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 }
--- src/main/java/com/uniajc/dao/MateriaDao.java
+++ src/main/java/com/uniajc/dao/MateriaDao.java
@@ -1,5 +1,33 @@
 package com.uniajc.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import com.uniajc.config.ConexionDatabase;
+import com.uniajc.modelo.Materia;
+
 public class MateriaDao {
-    
+
+    public void guardarMateria(Materia materia) {
+
+        try {
+            Connection conexion = ConexionDatabase.conectar();
+
+            String sql = "INSERT INTO materia VALUES (?, ?, ?)";
+
+            PreparedStatement ps = conexion.prepareStatement(sql);
+
+            ps.setInt(1, materia.getId_materia());
+            ps.setString(2, materia.getNombre_materia());
+            ps.setInt(3, materia.getCreditos());
+
+            ps.executeUpdate();
+
+            System.out.println("Materia guardada correctamente");
+
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 }
